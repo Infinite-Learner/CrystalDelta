db4c2aad486be9fbe11a75f341bc47cd2ae87632
Infinite-Learner <msdeepakpious@gmail.com>
2024-12-29 20:01:40 -0800

    simple modifications

--- Project E-Commerce/backend/server.js
+++ Project E-Commerce/backend/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 app.use(cors());
 app.use(bodyParser.json());
-express.static(path.join(__dirname, 'frontend'));
+express.static(path.join(__dirname, 'frontend','assets','styles'));
 
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
@@ -15,4 +15,5 @@ app.use('/api/auth', authRoutes);
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+}
+); 
